96247f4f5e24456868a7bb24941625583b3c97d7
Matthew David Elgert <mdelgert@yahoo.com>
2023-03-20 09:47:41 -0400

    Update demo7.py

--- demo7.py
+++ demo7.py
@@ -1,2 +1,3 @@
 #https://www.datacamp.com/tutorial/sqlalchemy-tutorial-examples
 #https://realpython.com/python-sqlite-sqlalchemy/#example-program
+#https://hackersandslackers.com/python-database-management-sqlalchemy/
